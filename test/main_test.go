e1f33175c6743a34cd02eb9a541d943e43b24288
Xinshuo Lei <leixshuo@gmail.com>
2023-09-06 17:53:47 -0500

    add some test cases

--- test/main_test.go
+++ test/main_test.go
@@ -2,9 +2,8 @@ package test
 
 import (
 	"fmt"
-	"math/rand"
 	"net/rpc"
-	"strings"
+	"os"
 	"testing"
 
 	"github.com/xinshuoLei/cs425-mp1/grep"
@@ -14,56 +13,163 @@ var ips = grep.LoadIps()
 var clients = make([]*rpc.Client, len(ips))
 var LogServiceNew = &LogService{LogFileDir: "./logs"}
 
-func TestGrepBasic(t *testing.T) {
+// This function runs before any test functions are executed.
+func TestMain(m *testing.M) {
 
-	defer grep.CloseClients(clients)
+	// Run the actual tests.
+	exitCode := m.Run()
 
-	var localFileNames []string
+	// clean up
+	fmt.Println("Clean up")
+	grep.CloseClients(clients)
+	os.Exit(exitCode)
+}
+
+// Each machine contains a log file from 2-10 lines
+// the pattern is 111 and has a 0.1 probablity to appear on each line
+func TestGrepRare(t *testing.T) {
+
 	pattern := "111"
 
-	// TODO: refactor log generation & write into a function so it can be reused
-	for index, ip := range ips {
-		// try start first time connection / reconnect for broken ones
-		if clients[index] == nil {
-			c, err := rpc.DialHTTP("tcp", ip)
-			if err == nil {
-				clients[index] = c
-			}
-		}
-
-		fileContent := ""
-		lineNumbers := 2 + rand.Intn(8)
-
-		for i := 0; i < lineNumbers; i++ {
-			fileContent += GenerateRandomString(10)
-			addPattern := rand.Intn(3)
-			if addPattern == 1 {
-				fileContent += pattern
-			}
-			fileContent += "\n"
-		}
-
-		fileName := strings.Split(ip, ":")[1]
-		fileName += ".txt"
-		args := Args{FileContent: fileContent, FileName: fileName}
-		var result string
-
-		if clients[index] != nil {
-			err := clients[index].Call("LogService.GenerateLog", args, &result)
-			if err != nil {
-				t.Fatal("Generate log error:", err)
-			}
-
-			// write a copy of the generated test log file to a local folder
-			localFilePath := fmt.Sprintf("./test_logs/%s", fileName)
-			writeErr := writeToFile(localFilePath, fileContent)
-			if writeErr != nil {
-				t.Fatal("Error writing log file to local folder", writeErr)
-			}
-
-			localFileNames = append(localFileNames, fileName)
-		}
+	randomFileArgs := RandomFileArgs{
+		minLineNumber:      2,
+		maxLineNumber:      10,
+		minLineLength:      5,
+		maxLineLength:      15,
+		pattern:            pattern,
+		patternProbability: 0.1,
+		machineProbability: 1,
 	}
+
+	localFileNames := PrepareLogFiles(randomFileArgs, ips, clients, t)
+
+	distributedRes := grep.GrepAllMachines(ips, clients, pattern)
+
+	localRes, status := localGrepMultipleFiles(pattern, localFileNames)
+	if status != "ok" {
+		t.Fatal("Local grep error:", status)
+	}
+
+	if distributedRes != localRes {
+		t.Fatalf("Incorrect result. Should be %s\n but got %s\n", localRes, distributedRes)
+	} else {
+		fmt.Printf("Got correct result:\n%s", localRes)
+	}
+}
+
+// Each machine contains a log file from 10-20 lines
+// the pattern is 123 and has a 0.8 probablity to appear on each line
+func TestGrepFrequentPattern(t *testing.T) {
+
+	pattern := "123"
+
+	randomFileArgs := RandomFileArgs{
+		minLineNumber:      10,
+		maxLineNumber:      20,
+		minLineLength:      5,
+		maxLineLength:      15,
+		pattern:            pattern,
+		patternProbability: 0.8,
+		machineProbability: 1,
+	}
+
+	localFileNames := PrepareLogFiles(randomFileArgs, ips, clients, t)
+
+	distributedRes := grep.GrepAllMachines(ips, clients, pattern)
+
+	localRes, status := localGrepMultipleFiles(pattern, localFileNames)
+	if status != "ok" {
+		t.Fatal("Local grep error:", status)
+	}
+
+	if distributedRes != localRes {
+		t.Fatalf("Incorrect result. Should be %s\n but got %s\n", localRes, distributedRes)
+	} else {
+		fmt.Printf("Got correct result:\n%s", localRes)
+	}
+}
+
+// Each machine contains a log file from 5-20 lines
+// the pattern is 123456 and has a 0.5 probablity to appear on each line
+func TestGrepSomewhatFrequentPattern(t *testing.T) {
+
+	pattern := "123456"
+
+	randomFileArgs := RandomFileArgs{
+		minLineNumber:      5,
+		maxLineNumber:      20,
+		minLineLength:      5,
+		maxLineLength:      15,
+		pattern:            pattern,
+		patternProbability: 0.5,
+		machineProbability: 1,
+	}
+
+	localFileNames := PrepareLogFiles(randomFileArgs, ips, clients, t)
+
+	distributedRes := grep.GrepAllMachines(ips, clients, pattern)
+
+	localRes, status := localGrepMultipleFiles(pattern, localFileNames)
+	if status != "ok" {
+		t.Fatal("Local grep error:", status)
+	}
+
+	if distributedRes != localRes {
+		t.Fatalf("Incorrect result. Should be %s\n but got %s\n", localRes, distributedRes)
+	} else {
+		fmt.Printf("Got correct result:\n%s", localRes)
+	}
+}
+
+// Each machine contains a log file from 5-20 lines
+// the pattern is 1321 and has a 0.7 probablity to appear on each machine
+func TestPatternOnSomeMachines(t *testing.T) {
+
+	pattern := "1321"
+
+	randomFileArgs := RandomFileArgs{
+		minLineNumber:      5,
+		maxLineNumber:      20,
+		minLineLength:      5,
+		maxLineLength:      15,
+		pattern:            pattern,
+		patternProbability: 0.5,
+		machineProbability: 0.7,
+	}
+
+	localFileNames := PrepareLogFiles(randomFileArgs, ips, clients, t)
+
+	distributedRes := grep.GrepAllMachines(ips, clients, pattern)
+
+	localRes, status := localGrepMultipleFiles(pattern, localFileNames)
+	if status != "ok" {
+		t.Fatal("Local grep error:", status)
+	}
+
+	if distributedRes != localRes {
+		t.Fatalf("Incorrect result. Should be %s\n but got %s\n", localRes, distributedRes)
+	} else {
+		fmt.Printf("Got correct result:\n%s", localRes)
+	}
+}
+
+// Each machine contains a log file from 5-20 lines
+// the pattern is 178 and only appear on one machine
+func TestPatternOnOneMachine(t *testing.T) {
+
+	pattern := "178"
+
+	randomFileArgs := RandomFileArgs{
+		minLineNumber:      5,
+		maxLineNumber:      20,
+		minLineLength:      5,
+		maxLineLength:      15,
+		pattern:            pattern,
+		patternProbability: 0.5,
+		machineProbability: -1, // a special case
+	}
+
+	localFileNames := PrepareLogFiles(randomFileArgs, ips, clients, t)
 
 	distributedRes := grep.GrepAllMachines(ips, clients, pattern)
 
--- test/test_util.go
+++ test/test_util.go
@@ -4,10 +4,13 @@ import (
 	"bytes"
 	"fmt"
 	"math/rand"
+	"net/rpc"
 	"os"
 	"os/exec"
 	"path/filepath"
 	"runtime"
+	"strings"
+	"testing"
 )
 
 var (
@@ -22,6 +25,16 @@ type LogService struct {
 type Args struct {
 	FileContent string
 	FileName    string
+}
+
+type RandomFileArgs struct {
+	minLineNumber      int
+	maxLineNumber      int
+	minLineLength      int
+	maxLineLength      int
+	pattern            string
+	patternProbability float64
+	machineProbability float64
 }
 
 func (service *LogService) GenerateLog(args *Args, reply *string) error {
@@ -68,4 +81,74 @@ func GenerateRandomString(length int) string {
 		b[i] = letterBytes[rand.Intn(len(letterBytes))]
 	}
 	return string(b)
+}
+
+func GenerateRandomFile(minLineNumber int, maxLineNumber int, minLineLength int, maxLineLength int,
+	pattern string, patternProbability float64) string {
+	fileContent := ""
+	lineNumbers := minLineNumber + rand.Intn(maxLineNumber-minLineNumber)
+
+	for i := 0; i < lineNumbers; i++ {
+		lineLength := minLineLength + rand.Intn(maxLineLength-minLineLength)
+		currentLine := GenerateRandomString(lineLength)
+		if rand.Float64() < patternProbability {
+			insertPosition := rand.Intn(lineLength)
+			currentLine = currentLine[:insertPosition] + pattern + currentLine[insertPosition:]
+		}
+		fileContent += currentLine
+		fileContent += "\n"
+	}
+	return fileContent
+}
+
+func PrepareLogFiles(args RandomFileArgs, ips []string, clients []*rpc.Client, t *testing.T) []string {
+
+	var localFileNames []string
+	oneMachineHasPattern := false
+	for index, ip := range ips {
+		// try start first time connection / reconnect for broken ones
+		if clients[index] == nil {
+			c, err := rpc.DialHTTP("tcp", ip)
+			if err == nil {
+				clients[index] = c
+			}
+		}
+
+		if clients[index] == nil {
+			continue
+		}
+
+		// check if pattern should appear on machine
+		if rand.Float64() >= args.machineProbability && args.machineProbability != -1 {
+			args.patternProbability = 0
+		}
+
+		if args.machineProbability == -1 && oneMachineHasPattern {
+			args.patternProbability = 0
+		} else {
+			oneMachineHasPattern = true
+		}
+
+		fileContent := GenerateRandomFile(args.minLineNumber, args.maxLineNumber,
+			args.minLineLength, args.maxLineLength, args.pattern, args.patternProbability)
+		fileName := strings.Split(ip, ":")[1]
+		fileName += ".txt"
+		args := Args{FileContent: fileContent, FileName: fileName}
+		var result string
+
+		err := clients[index].Call("LogService.GenerateLog", args, &result)
+		if err != nil {
+			t.Fatal("Generate log error:", err)
+		}
+
+		// write a copy of the generated test log file to a local folder
+		localFilePath := fmt.Sprintf("./test_logs/%s", fileName)
+		writeErr := writeToFile(localFilePath, fileContent)
+		if writeErr != nil {
+			t.Fatal("Error writing log file to local folder", writeErr)
+		}
+
+		localFileNames = append(localFileNames, fileName)
+	}
+	return localFileNames
 }
