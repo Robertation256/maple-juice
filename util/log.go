21900296d9151d78a0d37ef69c2c665d4dc44439
Xinshuo Lei <leixshuo@gmail.com>
2023-09-17 01:16:14 -0500

    actually including the changes

---
+++ util/log.go
@@ -0,0 +1,111 @@
+package util
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"sync"
+)
+
+type LogEntry struct {
+	Type    string
+	Message string
+}
+
+type Logger struct {
+	logFile *os.File
+	log     *log.Logger
+	logChan chan LogEntry
+	wg      sync.WaitGroup
+}
+
+func NewLogger(logFilePath string, bufferSize int) (*Logger, error) {
+	// create if file does not exist, append otherwise
+	logFile, err := os.OpenFile(logFilePath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		return nil, err
+	}
+
+	logger := &Logger{
+		logFile: logFile,
+		// create a new logger with two flags and no prefix
+		log:     log.New(logFile, "", log.Ldate|log.Ltime),
+		logChan: make(chan LogEntry, bufferSize),
+	}
+
+	logger.wg.Add(1)
+	go logger.processLogs()
+
+	return logger, nil
+}
+
+func (l *Logger) LogJoin(message string) {
+	entry := LogEntry{
+		Type:    "JOIN",
+		Message: message,
+	}
+	l.logChan <- entry
+}
+
+func (l *Logger) LogLeave(message string) {
+	entry := LogEntry{
+		Type:    "LEAVE",
+		Message: message,
+	}
+	l.logChan <- entry
+}
+
+func (l *Logger) LogFail(message string) {
+	entry := LogEntry{
+		Type:    "FAIL",
+		Message: message,
+	}
+	l.logChan <- entry
+}
+
+func (l *Logger) Close() {
+	close(l.logChan)
+	l.wg.Wait()
+	l.logFile.Close()
+}
+
+func (l *Logger) processLogs() {
+	// use waitgroup to ensure all logs are written before closing
+	defer l.wg.Done()
+
+	for entry := range l.logChan {
+		logLine := fmt.Sprintf("[%s] %s ", entry.Type, entry.Message)
+		l.log.Println(logLine)
+	}
+}
+
+// simple test
+// uncomment this and change package from log to main to test
+
+// func main() {
+// 	logger, err := NewLogger("example.log", 100)
+// 	if err != nil {
+// 		fmt.Printf("Error creating logger: %v\n", err)
+// 		return
+// 	}
+
+// 	// simulate multiple concurrent logging requests
+// 	for i := 1; i <= 10; i++ {
+// 		go func(index int) {
+// 			message := fmt.Sprintf("node %d", index)
+// 			logger.LogJoin(message)
+// 			if index == 5 {
+// 				time.Sleep(6 * time.Second)
+// 				logger.LogLeave("node 5")
+// 			}
+// 		}(i)
+// 	}
+
+// 	time.Sleep(3 * time.Second)
+
+// 	logger.LogFail("node 3")
+
+// 	fmt.Scanln()
+// 	// logger will be closed (no more logs recorded) when user hit enter
+// 	logger.Close()
+// }
--- util/util.go
+++ util/util.go
@@ -1,16 +1,14 @@
 package util
 
 import (
+	"fmt"
+	"log"
+	"net"
 	"strconv"
 	"strings"
-	"errors"
-	"fmt"
-	"net"
-	"log"
 )
 
-
-func Prompt(title string, ret *string, isValidInput func(string) bool){
+func Prompt(title string, ret *string, isValidInput func(string) bool) {
 	var input string
 	for {
 		fmt.Println(title)
@@ -26,7 +24,7 @@ func Prompt(title string, ret *string, isValidInput func(string) bool){
 func IsValidAddress(in string) bool {
 	elems := strings.Split(in, ":")
 	if len(elems) != 2 {
-		return false 
+		return false
 	}
 	return IsValidIp(elems[0]) && IsValidPort(elems[1])
 }
@@ -47,16 +45,16 @@ func IsValidIp(in string) bool {
 			return false
 		}
 	}
-	return true 
+	return true
 }
 
 func getOutboundIp() [4]uint8 {
 	var ret [4]uint8
 	conn, _ := net.Dial("udp4", "8.8.8.8:80")
 	defer conn.Close()
-	ip := strings.Split(conn.LocalAddr().String(),":")[0]
+	ip := strings.Split(conn.LocalAddr().String(), ":")[0]
 
-	elems := strings.Split(ip,".")
+	elems := strings.Split(ip, ".")
 	for i := range ret {
 		v, err := strconv.Atoi(elems[i])
 		if err != nil {
@@ -67,62 +65,59 @@ func getOutboundIp() [4]uint8 {
 	return ret
 }
 
+// func ExtractLineCount(str string) (int32, error) {
+// 	str = strings.Trim(str, " \r\n")
+// 	if len(str) == 0 {
+// 		return 0, nil
+// 	}
+// 	values := strings.Split(str, ":")
+// 	if (len(values) < 2){
+// 		return -1, errors.New("Incorrect input format")
+// 	}
+// 	countVal := strings.Trim(values[1], " \r\n")
+// 	ret, err := strconv.Atoi(countVal)
+// 	if (err != nil){
+// 		return -1, err
+// 	}
+// 	return int32(ret), nil;
+// }
 
-func ExtractLineCount(str string) (int32, error) {
-	str = strings.Trim(str, " \r\n")
-	if len(str) == 0 {
-		return 0, nil
-	}
-	values := strings.Split(str, ":")
-	if (len(values) < 2){
-		return -1, errors.New("Incorrect input format")
-	}
-	countVal := strings.Trim(values[1], " \r\n")
-	ret, err := strconv.Atoi(countVal)
-	if (err != nil){
-		return -1, err
-	}
-	return int32(ret), nil;
-}
+// func ParseUserInput(input string) ([]string, error) {	// parse out the options and the pattern
+// 	containsRequiredFlag := false
+// 	ret := make([]string,0)
+// 	if len(input) < 5 || input[:4] != "grep" {
+// 		return nil, errors.New("Must be a grep command")
+// 	}
+// 	for i := 4 ; i<len(input)-1; {
+// 		if input[i]=='-' {
+// 			if input[i+1] == 'c'{
+// 				containsRequiredFlag = true
+// 			}
+// 			// ignore some flag that might break stuff
+// 			if input[i+1] != 'H' && input[i+1] != 'f' && input[i+1] != 'q' {
+// 				ret = append(ret, "-"+ string(input[i+1]))
+// 			}
+// 			i += 2
+// 		} else if input[i]!=' ' {	// a pattern
+// 			ret = append(ret, input[i:])
+// 			break;
+// 		} else {
+// 			i++
+// 		}
+// 	}
 
+// 	if !containsRequiredFlag {
+// 		return nil, errors.New("Grep command must carry -c option.")
+// 	}
 
-func ParseUserInput(input string) ([]string, error) {	// parse out the options and the pattern
-	containsRequiredFlag := false
-	ret := make([]string,0)
-	if len(input) < 5 || input[:4] != "grep" {
-		return nil, errors.New("Must be a grep command")
-	}
-	for i := 4 ; i<len(input)-1; {
-		if input[i]=='-' {
-			if input[i+1] == 'c'{
-				containsRequiredFlag = true
-			}
-			// ignore some flag that might break stuff
-			if input[i+1] != 'H' && input[i+1] != 'f' && input[i+1] != 'q' {
-				ret = append(ret, "-"+ string(input[i+1]))
-			}
-			i += 2
-		} else if input[i]!=' ' {	// a pattern
-			ret = append(ret, input[i:])
-			break;
-		} else {
-			i++
-		}
-	}
+// 	for j := 0; j < len(ret); j++ {
+// 		ret[j] = strings.Trim(ret[j], " \n\r")
+// 	}
 
-	if !containsRequiredFlag {
-		return nil, errors.New("Grep command must carry -c option.")
-	}
+// 	pattern := ret[len(ret)-1]
+// 	if(len(pattern)>1 && pattern[0]=='"' && pattern[len(pattern)-1]=='"'){
+// 		ret[len(ret)-1] = pattern[1:len(pattern)-1]	// strip enclosing quotes
+// 	}
 
-	
-	for j := 0; j < len(ret); j++ {
-		ret[j] = strings.Trim(ret[j], " \n\r")
-	} 
-
-	pattern := ret[len(ret)-1]
-	if(len(pattern)>1 && pattern[0]=='"' && pattern[len(pattern)-1]=='"'){
-		ret[len(ret)-1] = pattern[1:len(pattern)-1]	// strip enclosing quotes
-	}
-	
-	return ret, nil
-}
+// 	return ret, nil
+// }
