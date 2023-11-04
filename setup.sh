78e95515259a667fbfd6366ce3696796f8c0e2cc
Robertation <67719835+Robertation256@users.noreply.github.com>
2023-11-03 19:44:23 -0500

    Update setup script

--- setup.sh
+++ setup.sh
@@ -13,11 +13,11 @@ echo "LEADER_ELECTION_SERVER_PORT=8003" >> config.txt
 echo "LEADER_ELECTION_QUORUM_SIZE=2" >> config.txt
 
 
-echo "FILE_METADATA_SERVER_PORT=8004" >> config.txt
-echo "FILE_SERVER_PORT=8005" >> config.txt
 echo "REPLICATION_FACTOR=2" >> config.txt
 
-echo "LOG_SERVER_PORT=8000" >> config.txt
+echo "RPC_SERVER_PORT=8004" >> config.txt
+
+
 echo "LOG_FILE_NAME=log" >> config.txt
 echo "LOG_SERVER_ID=vm$1" >> config.txt
 echo "LOG_SERVER_HOSTNAMES=fa23-cs425-3801.cs.illinois.edu,fa23-cs425-3802.cs.illinois.edu,fa23-cs425-3803.cs.illinois.edu,fa23-cs425-3804.cs.illinois.edu,fa23-cs425-3805.cs.illinois.edu,fa23-cs425-3806.cs.illinois.edu,fa23-cs425-3807.cs.illinois.edu,fa23-cs425-3808.cs.illinois.edu,fa23-cs425-3809.cs.illinois.edu,fa23-cs425-3810.cs.illinois.edu" >> config.txt
