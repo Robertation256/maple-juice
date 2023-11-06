e1f4727f32388289c37fb047ebc4a18a6e5f0ab0
Robertation <67719835+Robertation256@users.noreply.github.com>
2023-11-05 23:25:35 -0600

    Update reademe

--- setup.sh
+++ setup.sh
@@ -12,10 +12,10 @@ echo "INTRODUCER_PORT=8002" >> config.txt
 
 
 echo "LEADER_ELECTION_SERVER_PORT=8003" >> config.txt
-echo "LEADER_ELECTION_QUORUM_SIZE=2" >> config.txt
+echo "LEADER_ELECTION_QUORUM_SIZE=6" >> config.txt
 
 
-echo "REPLICATION_FACTOR=2" >> config.txt
+echo "REPLICATION_FACTOR=4" >> config.txt
 
 echo "RPC_SERVER_PORT=8004" >> config.txt
 echo "FILE_SERVER_RECEIVE_PORT=8005" >> config.txt
@@ -24,7 +24,6 @@ echo "DFS_CLIENT_RECEIVE_PORT=8006" >> config.txt
 echo "LOG_FILE_NAME=log" >> config.txt
 echo "LOG_SERVER_ID=vm$1" >> config.txt
 echo "SERVER_HOSTNAMES=fa23-cs425-3801.cs.illinois.edu,fa23-cs425-3802.cs.illinois.edu,fa23-cs425-3803.cs.illinois.edu,fa23-cs425-3804.cs.illinois.edu,fa23-cs425-3805.cs.illinois.edu,fa23-cs425-3806.cs.illinois.edu,fa23-cs425-3807.cs.illinois.edu,fa23-cs425-3808.cs.illinois.edu,fa23-cs425-3809.cs.illinois.edu,fa23-cs425-3810.cs.illinois.edu" >> config.txt
-echo "SSH_USERNAME=$2" >> config.txt
-echo "SSH_PASSWORD=$3" >> config.txt
+
 
 
