4d4d5083b96ef57f369fc62f8a6f7eb95103c3b1
xinshuo3 <xinshuo3@illinois.com>
2023-12-02 20:38:38 -0600

    add demo executables

---
+++ demo/juice1/juice1.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+)
+
+/*
+
+Juice (key, value):
+	output(key, |value|)
+
+*/
+
+func main() {
+	log.SetOutput(os.Stderr)
+	homedir, _ := os.UserHomeDir()
+	nodeManagerFileDir := homedir + "/mr_node_manager/"
+
+	// define flags
+	inputFileFlag := flag.String("in", "", "Input filename")
+	outputFileFlag := flag.String("dest", "", "Output filename")
+	flag.Parse()
+
+	// check if required flags are provided
+	if *inputFileFlag == "" || *outputFileFlag == "" {
+		log.Fatal("Usage: go run join_juice.go -in <inputfile> -dest <outputfile>")
+	}
+
+	// Read input file
+	file, err := os.Open(*inputFileFlag)
+	if err != nil {
+		log.Fatal("Error opening input file:", err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	if err := scanner.Err(); err != nil {
+		log.Fatal("Error reading input file:", err)
+	}
+
+	splittedInputFilename := strings.Split(*inputFileFlag, "-")
+	key := splittedInputFilename[len(splittedInputFilename)-1]
+
+	// write output to file
+	outputFile, err := os.Create(nodeManagerFileDir + *outputFileFlag)
+	if err != nil {
+		log.Fatal("Error creating output file:", err)
+	}
+	defer outputFile.Close()
+
+	// process each line and get the count of records
+	count := 0
+	for scanner.Scan() {
+		line := scanner.Text()
+		if strings.TrimSpace(line) == "1" {
+			count += 1
+		}
+	}
+
+	outputFile.WriteString(fmt.Sprintf("%s, %d", key, count))
+
+	fmt.Println(*outputFileFlag)
+	os.Exit(0)
+}
